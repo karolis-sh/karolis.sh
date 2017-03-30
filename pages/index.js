15d72bb3e87376fb43d1de27a57d8750c4e1c210
Karolis <pastas.k@gmail.com>
2017-03-30 22:30:19 +0300

    initial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default () =>
+  <html>
+    <Head>
+      <title>My page title</title>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
+    <body>
+      Sup!
+    </body>
+  </html>;
