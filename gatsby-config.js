4bda4afb8a87c7e7efd41ee450eb67b77569c737
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-06-18 21:37:14 +0300

    feat: gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const rootDir = 'public';
+
 module.exports = {
   plugins: [
     'gatsby-plugin-preact',
@@ -17,55 +19,18 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: `gatsby-plugin-offline`,
       options: {
-        name: 'karolis.sh',
-        short_name: 'karolis.sh',
-        background_color: '#000',
-        theme_color: '#000',
-        display: 'standalone',
-        lang: 'en-US',
-        start_url: '.',
-        icons: [
-          {
-            src: '/android-icon-36x36.png',
-            sizes: '36x36',
-            type: 'image/png',
-            density: '0.75',
-          },
-          {
-            src: '/android-icon-48x48.png',
-            sizes: '48x48',
-            type: 'image/png',
-            density: '1.0',
-          },
-          {
-            src: '/android-icon-72x72.png',
-            sizes: '72x72',
-            type: 'image/png',
-            density: '1.5',
-          },
-          {
-            src: '/android-icon-96x96.png',
-            sizes: '96x96',
-            type: 'image/png',
-            density: '2.0',
-          },
-          {
-            src: '/android-icon-144x144.png',
-            sizes: '144x144',
-            type: 'image/png',
-            density: '3.0',
-          },
-          {
-            src: '/android-icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            density: '4.0',
-          },
+        staticFileGlobs: [
+          `${rootDir}/**/*.{woff2}`,
+          `${rootDir}/__static-commons-*js`,
+          `${rootDir}/__static-app-*js`,
+          `${rootDir}/index.html`,
+          `${rootDir}/manifest.json`,
+          `${rootDir}/manifest.webmanifest`,
+          `${rootDir}/offline-plugin-app-shell-fallback/index.html`,
         ],
       },
     },
-    'gatsby-plugin-offline',
   ],
 };
