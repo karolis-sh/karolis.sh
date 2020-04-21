0c0be90e9aab729e1b84ea26d064f60d15010e4d
Karolis Šarapnickis <pastas.k@gmail.com>
2020-04-22 00:57:22 +0300

    fix: update content

--- src/data.js
+++ src/data.js
@@ -2,13 +2,13 @@ export default {
   metaTitle: 'karolis.sh',
   metaDescription: 'My personal page - who I am, my resume and social links.',
   cvFile: 'Karolis_CV.pdf',
-  title: 'Software Developer - Team Lead',
+  title: 'Software Developer - Front-end Tech Lead',
   introText: `
     I build high quality web/mobile/desktop apps using JavaScript technologies.
     I have experience working with the whole stack but I am more of a Front-end oriented Software Developer.
   `,
   skillsTitle: 'Key technologies I am experienced with:',
-  skills: ['JavaScript', 'React', 'Redux', 'HTML', 'CSS', 'Node', 'Electron', 'React-Native'],
+  skills: ['JavaScript', 'React', 'Redux', 'HTML', 'CSS', 'Node', 'TypeScript'],
   socialLinks: [
     {
       icon: 'gmail',
