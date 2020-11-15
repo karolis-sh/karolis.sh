894a60a1ec56f7008c5500b580e2faae73bbc7c2
Karolis Šarapnickis <pastas.k@gmail.com>
2020-11-15 15:40:42 +0200

    chore: remove unused breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
   // Defaults:
   // https://github.com/tailwindlabs/tailwindcss/blob/e7d053444a95f82eeb4946fc3550dba62d31c75c/stubs/defaultConfig.stub.js
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
